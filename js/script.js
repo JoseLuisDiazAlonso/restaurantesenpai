f22f61d8ada7d11e1037069fddd03b0976707323
Jose Luis Díaz Alonso <134778793+JoseLuisDiazDev@users.noreply.github.com>
2023-12-28 20:26:26 +0100

    Este es el JavaScript de la página Inicio

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+/**Creamos la función necesaria para cambiar las imágenes de la galería. */
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    var imagenes = document.querySelectorAll('.galeria img');
+    var indice = 0;
+
+    function cambiarImagen() {
+        imagenes[indice].classList.remove('active');
+        indice = (indice + 1) % imagenes.length;
+        imagenes[indice].classList.add('active');
+    }
+
+    setInterval(cambiarImagen, 4000) //Marcamos el cambio de imagen cada 4 segundos
+
+});
